777c26c6026e0a419095b1ee452de83601d61a2e
Pim <pim.veldhuisen@gmail.com>
2016-06-12 12:14:55 +0200

    Fix an error when quering multichain blocks for unknown peers

--- Tribler/community/multichain/community.py
+++ Tribler/community/multichain/community.py
@@ -344,12 +344,20 @@ class MultiChainCommunity(Community):
         (statistics["self_total_up_mb"],
          statistics["self_total_down_mb"]) = self.persistence.get_total(self._public_key)
         latest_block = self.persistence.get_latest_block(self._public_key)
-        statistics["latest_block_insert_time"] = str(latest_block.insert_time)
-        statistics["latest_block_id"] = base64.encodestring(latest_block.hash_requester)
-        statistics["latest_block_requester_id"] = base64.encodestring(latest_block.public_key_requester)
-        statistics["latest_block_responder_id"] = base64.encodestring(latest_block.public_key_responder)
-        statistics["latest_block_up_mb"] = str(latest_block.up)
-        statistics["latest_block_down_mb"] = str(latest_block.down)
+        if latest_block:
+            statistics["latest_block_insert_time"] = str(latest_block.insert_time)
+            statistics["latest_block_id"] = base64.encodestring(latest_block.hash_requester)
+            statistics["latest_block_requester_id"] = base64.encodestring(latest_block.public_key_requester)
+            statistics["latest_block_responder_id"] = base64.encodestring(latest_block.public_key_responder)
+            statistics["latest_block_up_mb"] = str(latest_block.up)
+            statistics["latest_block_down_mb"] = str(latest_block.down)
+        else:
+            statistics["latest_block_insert_time"] = ""
+            statistics["latest_block_id"] = ""
+            statistics["latest_block_requester_id"] = ""
+            statistics["latest_block_responder_id"] = ""
+            statistics["latest_block_up_mb"] = ""
+            statistics["latest_block_down_mb"] = ""
         return statistics
 
     def _get_next_total(self, up, down):
